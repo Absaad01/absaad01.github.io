568f39c8b3b497daa49bfbde33f86d1e2c53959a
Abubakar Sadiq <87236067+Absaad01@users.noreply.github.com>
2022-07-02 08:37:28 +0100

    Update Calculator.js
    Task complete

--- Calculator.js
+++ Calculator.js
@@ -1,1 +1,26 @@
-//will work on it
+
+const firstNum = parseInt(prompt(' Enter First Number'))
+const secondNum = parseInt(prompt(' Enter Second Number'))
+const operator = parseInt(prompt(' Choose Operator(+,-,*,/)'))
+
+let result = 0
+if (operator == '+') {
+    result = firstNum + secondNum
+    alert(result);
+}
+else if (operator == '-') {
+    result = firstNum - secondNum
+    alert(result);
+}
+else if (operator == '*') {
+    result = firstNum * secondNum
+    alert(result);
+}
+
+else{
+    result = firstNum/secondNum
+    alert(result)
+}
+
+document.write(firstNum + operator + secondNum + "" + "=" + "" + result)
+console.log(firstNum + operator + secondNum + "" + "=" + "" + result)
